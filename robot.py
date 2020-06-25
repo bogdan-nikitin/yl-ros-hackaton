b9508b23c9d9150f3104dc16497454e3e948d5ea
Богдан <41363359+CurlyDen@users.noreply.github.com>
2020-06-25 12:59:09 +0300

    fix_vsego

--- robot.py
+++ robot.py
@@ -9,29 +9,29 @@ class TurtleMoverClass(object):
 
     def __init__(self):
         rospy.init_node("wild_west_python_node")
-        self.sub = rospy.Subscriber("/scan", LaserScan, self.scan_front)
+        self.sub = rospy.Subscriber("/scan", LaserScan, self.scan_cb)
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
         self.velocity = Twist()
-        rospy.spin()
         # дистанции, которые надо держать при движении вперёд и ключ к ним
         self.dist_x = 0
-        self.distanses_x = [20, 40, 20, 20, 20, 20, 40]
+        self.distanses_x = [0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4]
+        # скорости
+        self.lin = 0.1
+        self.ang = 0.1
+        # ключ скана
+        self.scan_key
+        rospy.spin()
 
-    def scan_front(self, msg):
-        # правый, левый и луч нормали(0 градусов)
-        self.r_ray = msg.ranges[10]
-        self.l_ray = msg.ranges[350]
-        self.dist_to_wall = msg.ranges[0]
+    def scan_cb(self, msg):
+        # правый, левый и луч нормали
+        self.r_ray = msg.ranges[(10, 275)[self.scan_key]]
+        self.l_ray = msg.ranges[(355, 265)[self.scan_key]]
+        self.dist_to_wall = msg.ranges[(0, 270)[self.scan_key]]
         # вызываем движение
-        self.mover()
-
-    def scan_side(self, msg):
-        # правый, левый и луч нормали(270 градусов)
-        self.r_ray = msg.ranges[280]
-        self.l_ray = msg.ranges[260]
-        self.dist_to_wall = msg.ranges[270]
-        # вызываем поворот
-        self.turner()
+        if self.scan_key:
+            self.turner()
+        else:
+            self.mover()
 
     def ros_publisher(self):
         self.pub.publish(self.velocity)
@@ -41,35 +41,37 @@ class TurtleMoverClass(object):
         if self.r_ray == self.l_ray:
             self.velocity.angular.z = 0
         elif self.r_ray < self.l_ray:
-            self.velocity.angular.z = 0.1
+            self.velocity.angular.z = self.ang
         elif self.r_ray > self.l_ray:
-            self.velocity.angular.z = -0.1
+            self.velocity.angular.z = -self.ang
 
         # русские вперёд!    
         if self.dist_to_wall == float('inf'):
             pass
-        elif self.dist_to_wall < self.dintances_x[self.dist_x] + 7.2:
-            # меняем функцию скана на сабе, стоим-ждём
+        elif self.dist_to_wall < self.dintanses_x[self.dist_x] + 0.072:
+            # стоим-ждём
             self.velocity.linear.x = 0
-            self.sub = rospy.Subscriber("/scan", LaserScan, self.scan_side)
+            self.velocity.angular.z = 0
             self.dist_x += 1
+            self.scan_key = 1 - self.scan_key
             press_to_start = input()
         else:
-            self.velocity.linear.x = 0.1
+            self.velocity.linear.x = self.lin
         self.ros_publisher()
 
     def turner(self):
         # поворачиваемс до состония, параллельного стене
         # ещё не учёл растоние
         if self.r_ray == self.l_ray:
-            # меняем функцию скана на сабе, стоим-ждём
+            # стоим-ждём
+            self.velocity.linear.x = 0
             self.velocity.angular.z = 0
-            self.sub = rospy.Subscriber("/scan", LaserScan, self.scan_front)
+            self.scan_key = 1 - self.scan_key
             press_to_start = input()
         elif self.r_ray < self.l_ray:
-            self.velocity.angular.z = 0.1
+            self.velocity.angular.z = self.ang
         elif self.r_ray > self.l_ray:
-            self.velocity.angular.z = -0.1
+            self.velocity.angular.z = -self.ang
          self.pub.publish(self.velocity)
 
 TurtleMoverClass()
