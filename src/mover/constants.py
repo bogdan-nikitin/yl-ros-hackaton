72792298ed6023e7695b831dfe30b35af13c131f
bogdan-nikitin <nikitin.bogdan333@yandex.ru>
2020-06-25 12:05:22 +0300

    Create constants

---
+++ src/mover/constants.py
@@ -0,0 +1,7 @@
+# Расстояние от центра LIDAR'а до краёв робота
+LIDAR_FRONT = 0.07
+LIDAR_BACK = 0.13
+LIDAR_LEFT = LIDAR_RIGHT = 0.1
+
+# Расстояние до стены, на котором надо остановиться
+DISTANCE_TO_WALL = 0.2
