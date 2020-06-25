edd8a13dd6df419d4c5f2ae4aca1bfab3638b9ae
u1228 <58815441+u1228@users.noreply.github.com>
2020-06-25 11:32:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-class TurtleMoverClass(object):
+class MoverClass(object):
     def __init__(self):
-        rospy.init_node("wild_west_node")
+        rospy.init_node("mover_node")
         self.sub = rospy.Subscriber("/scan", LaserScan, self.scan_cb)
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
         self.vel = Twist()
@@ -15,4 +15,4 @@ class TurtleMoverClass(object):
     def ros_publisher(self):
         self.pub.publish(self.vel)
 
-TurtleMoverClass()
+MoverClass()
