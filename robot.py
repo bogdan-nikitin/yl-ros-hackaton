b917b8a66830a3e0b58893e837bab0120b3945b3
Богдан <41363359+CurlyDen@users.noreply.github.com>
2020-06-25 13:17:59 +0300

    Add files via upload

--- robot.py
+++ robot.py
@@ -19,13 +19,14 @@ class TurtleMoverClass(object):
         self.lin = 0.1
         self.ang = 0.1
         # ключ скана
-        self.scan_key
+        self.scan_key = 0
+        
         rospy.spin()
 
     def scan_cb(self, msg):
         # правый, левый и луч нормали
-        self.r_ray = msg.ranges[(10, 275)[self.scan_key]]
-        self.l_ray = msg.ranges[(355, 265)[self.scan_key]]
+        self.r_ray = msg.ranges[(5, 315)[self.scan_key]]
+        self.l_ray = msg.ranges[(355, 225)[self.scan_key]]
         self.dist_to_wall = msg.ranges[(0, 270)[self.scan_key]]
         # вызываем движение
         if self.scan_key:
@@ -60,7 +61,7 @@ class TurtleMoverClass(object):
         self.ros_publisher()
 
     def turner(self):
-        # поворачиваемс до состония, параллельного стене
+        # поворачиваемся до состония, параллельного стене
         # ещё не учёл растоние
         if self.r_ray == self.l_ray:
             # стоим-ждём
@@ -72,6 +73,6 @@ class TurtleMoverClass(object):
             self.velocity.angular.z = self.ang
         elif self.r_ray > self.l_ray:
             self.velocity.angular.z = -self.ang
-         self.pub.publish(self.velocity)
+        self.ros_publisher()
 
 TurtleMoverClass()
